8b996f3cdb04ac1e8e8aa0492d4c8210acfe8985
Mateen Ulhaq <mateen.ulhaq@interdigital.com>
2022-06-03 20:24:10 -0700

    docs: compressai_train.utils.compressai example usage

--- compressai_train/utils/compressai/__main__.py
+++ compressai_train/utils/compressai/__main__.py
@@ -27,6 +27,20 @@
 # OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+"""Wrapper around CompressAI scripts to work more easily with CompressAI Train.
+
+Example usage:
+
+```bash
+python -m compressai_train.utils.compressai \
+    update_and_eval_model \
+    checkpoint \
+    --architecture="bmshj2018-factorized" \
+    --path="$HOME/data/runs/e4e6d4d5e5c59c69f3bd7be2/checkpoints/runner.best.pth" \
+    "$HOME/data/datasets/kodak/test"
+```
+"""
+
 import importlib
 import sys
 
