5e67e9fb854040bb9efce14cffd21d7dfb115e51
Mateen Ulhaq <mateen.ulhaq@interdigital.com>
2022-06-18 15:49:22 -0700

    style: isort

--- compressai_train/__init__.py
+++ compressai_train/__init__.py
@@ -27,15 +27,7 @@
 # OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from . import (
-    config,
-    models,
-    registry,
-    runners,
-    typing,
-    utils,
-    zoo,
-)
+from . import config, models, registry, runners, typing, utils, zoo
 
 __all__ = [
     "config",
