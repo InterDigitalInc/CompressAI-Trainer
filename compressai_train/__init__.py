b58b698216b84d1c0f737e1ccc33745f918417d4
Mateen Ulhaq <mateen.ulhaq@interdigital.com>
2022-12-20 22:49:53 -0800

    refactor: remove compressai_train.models and compressai_train.zoo

--- compressai_train/__init__.py
+++ compressai_train/__init__.py
@@ -27,15 +27,13 @@
 # OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from . import config, models, plot, registry, runners, typing, utils, zoo
+from . import config, plot, registry, runners, typing, utils
 
 __all__ = [
     "config",
-    "models",
     "plot",
     "registry",
     "runners",
     "typing",
     "utils",
-    "zoo",
 ]
